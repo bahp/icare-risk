f949349f141ef4d174a905c18430d9253aef406e
bahp <bernard.hernandez.perez@gmail.com>
2026-09-22 16:08:38 +0100

    ~ fix match_codes

--- src/icare_risk/clinphen/utils/filtering.py
+++ src/icare_risk/clinphen/utils/filtering.py
@@ -3,10 +3,8 @@ import pandas as pd
 def match_codes(series: pd.Series,
                 target_codes: list) -> pd.Series:
     """Returns a boolean mask for robust, case-insensitive string matching."""
-    # No need to upper, done when creating the phenotypes
-    # Inside the registry
-    #targets = [str(c).upper() for c in target_codes]
-    return series.astype(str).str.upper().isin(series)
+    targets = [str(c).upper() for c in target_codes] # Needed
+    return series.astype(str).str.upper().isin(targets)
 
 
 def extract_numeric(df: pd.DataFrame,
